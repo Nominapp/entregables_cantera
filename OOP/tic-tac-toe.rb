f5016bba70f743021d36259c73220139a2250936
CristianRojasBetancur <66922367+CristianRojasBetancur@users.noreply.github.com>
2022-08-05 01:45:11 -0500

    add class Main and HumanVsHuman mode

--- OOP/tic-tac-toe.rb
+++ OOP/tic-tac-toe.rb
@@ -18,9 +18,11 @@ Segundo implementar clases humano y computadora:
 =end
 
 class Board
-	@@board = [[1, 2, 3],
-						[4, 5, 6],
-						[7, 8, 9]]
+	@@board = [
+							[1, 2, 3],
+							[4, 5, 6],
+							[7, 8, 9]
+												]
 
 	@@freePositions = @@board.length * @@board[0].length
 	@@usedPositions = []
@@ -29,12 +31,16 @@ class Board
 
 		separator = "---+---+---+"
 
+		puts
+
 		@@board.each do |x|
 			x.each do |y|
 				print " #{y} |"
 			end
 			puts "\n#{separator}" 
 		end
+
+		puts
 	end
 
 	def update(position, symbol)
@@ -116,6 +122,19 @@ end
 
 class Human < Player
 
+	attr_reader :name
+
+	def initialize(name="Default")
+		super()
+		@name = name
+	end
+
+	def inputName
+		print "Ingrese el nombre del jugador #{self.id}: "
+		@name = gets.chomp
+		@name
+	end
+
 	def chooseSymbol
 		loop do
 
@@ -168,12 +187,14 @@ class Game
 
 	def play_PCvsHuman
 		board = Board.new
-		board.display_board
 		player1 = Human.new
 		pc1 = Computer.new
 
+		player1.inputName
+
 		symbolPlayer = player1.chooseSymbol
 		symbolPlayer == "X" ? symbolPC = "O" : symbolPC = "X"
+		board.display_board
 
 		while board.positionsCheck
 
@@ -181,14 +202,16 @@ class Game
 				
 				positionPlayer = player1.inputPosition
 
-				if Board.validatePosition(positionPlayer)
+				if Board.validatePosition(positionPlayer) && positionPlayer.to_i != 0 && positionPlayer <= 9 && positionPlayer > 0
 					board.update(positionPlayer, symbolPlayer)
+					board.display_board
 					if board.winCheck(symbolPlayer)
-						puts "El jugador #{player1.id} ha ganado :D"
+						bannerWin = Banner.new("#{player1.name.capitalize} ha ganado :D")
+						puts bannerWin
 					end
 					break
 				else
-					puts "La posición ingresada está en uso, intenta con otra :D"
+					puts "La posición ingresada no es válida o está en uso, intenta con otra :D"
 				end
 			end
 
@@ -217,13 +240,166 @@ class Game
 			if board.winCheck(symbolPC) || board.winCheck(symbolPlayer)
 				break
 			end 
-
 		end
-		
+	end
+
+	def play_HumanVsHuman
+		board = Board.new
+		player1 = Human.new
+		player2 = Human.new
+
+		player1.inputName
+		player2.inputName
+
+		symbolPlayer1 = player1.chooseSymbol
+		symbolPlayer1 == "X" ? symbolPlayer2 = "O" : symbolPlayer2 = "X"
 		board.display_board
+
+		while board.positionsCheck
+
+			print "Juega #{player1.name.capitalize} "
+
+			loop do
+				
+				positionPlayer1 = player1.inputPosition
+
+				if Board.validatePosition(positionPlayer1) && positionPlayer1.to_i != 0 && positionPlayer1 <= 9 && positionPlayer1 > 0
+					board.update(positionPlayer1, symbolPlayer1)
+					board.display_board
+					if board.winCheck(symbolPlayer1)
+						bannerWinPlayer1 = Banner.new("#{player1.name.capitalize} ha ganado :D")
+						puts bannerWinPlayer1
+					end
+					break
+				else
+					puts "La posición ingresada no es válida o está en uso, intenta con otra :D"
+				end
+			end
+
+			if board.winCheck(symbolPlayer2) || board.winCheck(symbolPlayer1)
+				break
+			end 
+
+			print "Juega #{player2.name.capitalize} "
+			
+			loop do
+				positionPlayer2 = player2.inputPosition
+
+				if Board.validatePosition(positionPlayer2) && positionPlayer2.to_i != 0 && positionPlayer2 <= 9 && positionPlayer2 > 0
+					
+					board.update(positionPlayer2, symbolPlayer2)
+					board.display_board
+					if board.winCheck(symbolPlayer2)
+						bannerWinPlayer2 = Banner.new("#{player2.name.capitalize} ha ganado :D")
+						puts bannerWinPlayer2
+						break
+					end 
+					break
+				else
+					puts "La posición ingresada no es válida o está en uso, intenta con otra :D"
+				end
+			end
+
+			if board.winCheck(symbolPlayer2) || board.winCheck(symbolPlayer1)
+				break
+			end 
+		end
 	end
 
 end
 
-partida = Game.new
-partida.play_PCvsHuman
+class Banner
+  def initialize(message)
+    @message = message
+  end
+
+  def to_s
+    [horizontal_rule, empty_line, message_line, empty_line, horizontal_rule].join("\n")
+  end
+
+  private
+
+  def horizontal_rule
+    "+#{'-'*(@message.length + 2)}+"
+  end
+
+  def empty_line
+    "|#{' '*(@message.length + 2)}|"
+  end
+
+  def message_line
+    "| #{@message} |"
+  end
+end
+
+class Main
+
+	game = Game.new
+
+	bannerStart = Banner.new('Bienvenido a Tic-Tac-Toe')
+	bannerExit = Banner.new("Fin del juego.")
+	banner1 = Banner.new("Computadora VS Computadora")
+	banner2 = Banner.new("Humano VS Computadora")
+	banner3 = Banner.new("Humano VS Humano")
+
+	puts bannerStart
+
+	puts "\nOpciones de juego: "
+
+	puts "\n1. Computadora VS Computadora."
+	puts "2. Humano VS Computadora."
+	puts "3. Humano VS Humano."
+
+	print "\nSeleccione que modo de juego desea (1, 2 o 3): "
+	gamemode = 0
+
+	loop do
+		gamemode = gets.chomp.to_i
+		if gamemode >= 1 && gamemode <= 3
+			break
+		else
+			print "Ingrese una opción válida (1, 2 o 3): "
+		end
+	end
+
+	case gamemode
+	when 1
+		print "\nHa seleccionado Computadora VS Computadora, presione enter para iniciar u otra tecla + enter para salir."
+		start = 'no'
+		start = gets.chomp
+		if start == ''
+			puts banner1
+			puts
+			game.play_PCvsPC
+		else
+			puts bannerExit
+		end
+
+	when 2
+	  print "\nHa seleccionado Humano VS Computadora, presione enter para iniciar u otra tecla + enter para salir."
+		start = 'no'
+		start = gets.chomp
+		if start == ''
+			puts banner2
+			puts
+			game.play_PCvsHuman
+		else
+			puts bannerExit
+		end
+	when 3
+	  print "\nHa seleccionado Humano VS Humano, presione enter para iniciar u otra tecla + enter para salir."
+		start = 'no'
+		start = gets.chomp
+		if start == ''
+			puts banner3
+			puts
+			game.play_HumanVsHuman
+		else
+			puts bannerExit
+		end
+	else
+	  "Selecciona una opción correcta."
+	end
+
+end
+
